03ab81217abb7cd868c8c932d97b74ad585dd71d
Yanke Song <ysong@g.harvard.edu>
2022-10-20 20:46:43 -0400

    helper

---
+++ dl_helpers_Yanke.py
@@ -0,0 +1,171 @@
+import torch
+from torch import nn
+from d2l import torch as d2l
+
+# Device Related
+def cpu():
+    return torch.device('cpu')
+def gpu(i=0):
+    return torch.device(f'cuda:{i}')
+def num_gpus(): 
+    return torch.cuda.device_count()
+def try_gpu(i=0):
+    if num_gpus() >= i + 1:
+        return gpu(i)
+    return cpu()
+def try_all_gpus(): 
+    return [gpu(i) for i in range(num_gpus())]
+
+# Wrapper function so that we can add more things after we create a class
+def add_to_class(Class):
+    def wrapper(obj):
+        setattr(Class, obj.__name__, obj)
+    return wrapper
+# class A:
+#     def __init__(self):
+#         self.b = 105
+# a = A()
+
+# @add_to_class(A)
+# def do(self):
+#     print('Class attribute "b" is', self.b)
+# a.do()
+
+# Hyperparameters
+class HyperParameters: 
+    def save_hyperparameters(self, ignore=[]):
+        raise NotImplemented
+# class B(d2l.HyperParameters):
+#     def __init__(self, a, b, c):
+#         self.save_hyperparameters(ignore=['c'])
+#         print('self.a =', self.a, 'self.b =', self.b)
+#         print('There is no self.c =', not hasattr(self, 'c'))
+
+# ProgressBoard
+class ProgressBoard(d2l.HyperParameters):
+    """Plot data points in animation."""
+    def __init__(self, xlabel=None, ylabel=None, xlim=None,
+                 ylim=None, xscale='linear', yscale='linear',
+                 ls=['-', '--', '-.', ':'], colors=['C0', 'C1', 'C2', 'C3'],
+                 fig=None, axes=None, figsize=(3.5, 2.5), display=True):
+        self.save_hyperparameters()
+
+    def draw(self, x, y, label, every_n=1):
+        raise NotImplemented
+
+# Models
+class Module(nn.Module, d2l.HyperParameters):
+    def __init__(self, plot_train_per_epoch=2, plot_valid_per_epoch=1):
+        super().__init__()
+        self.save_hyperparameters()
+        self.board = ProgressBoard()
+    def loss(self, y_hat, y):
+        raise NotImplementedError
+
+    def forward(self, X):
+        assert hasattr(self, 'net'), 'Neural network is defined'
+        return self.net(X)
+
+    def plot(self, key, value, train):
+        """Plot a point in animation."""
+        assert hasattr(self, 'trainer'), 'Trainer is not inited'
+        self.board.xlabel = 'epoch'
+        if train:
+            x = self.trainer.train_batch_idx / \
+                self.trainer.num_train_batches
+            n = self.trainer.num_train_batches / \
+                self.plot_train_per_epoch
+        else:
+            x = self.trainer.epoch + 1
+            n = self.trainer.num_val_batches / \
+                self.plot_valid_per_epoch
+        self.board.draw(x, value.to(d2l.cpu()).detach().numpy(),
+                        ('train_' if train else 'val_') + key,
+                        every_n=int(n))
+
+    def training_step(self, batch):
+        l = self.loss(self(*batch[:-1]), batch[-1])
+        self.plot('loss', l, train=True)
+        return l
+
+    def validation_step(self, batch):
+        l = self.loss(self(*batch[:-1]), batch[-1])
+        self.plot('loss', l, train=False)
+
+    def configure_optimizers(self):
+        raise NotImplementedError
+
+# DataModule
+class DataModule(d2l.HyperParameters):
+    def __init__(self, root='../data', num_workers=2):
+        self.save_hyperparameters()
+
+    def get_dataloader(self, train):
+        raise NotImplementedError
+
+    def train_dataloader(self):
+        return self.get_dataloader(train=True)
+
+    def val_dataloader(self):
+        return self.get_dataloader(train=False)
+    
+    def get_tensorloader(self, tensors, train, indices=slice(0, None)):
+        tensors = tuple(a[indices] for a in tensors)
+        dataset = torch.utils.data.TensorDataset(*tensors)
+        return torch.utils.data.DataLoader(dataset, self.batch_size,
+                                        shuffle=train)
+
+
+# Trainer
+class Trainer(d2l.HyperParameters):
+    def __init__(self, max_epochs, num_gpus=0, gradient_clip_val=0):
+        self.save_hyperparameters()
+        self.gpus = [d2l.gpu(i) for i in range(min(num_gpus, d2l.num_gpus()))]
+
+    def prepare_data(self, data):
+        self.train_dataloader = data.train_dataloader()
+        self.val_dataloader = data.val_dataloader()
+        self.num_train_batches = len(self.train_dataloader)
+        self.num_val_batches = (len(self.val_dataloader)
+                                if self.val_dataloader is not None else 0)
+
+    def prepare_batch(self, batch):
+        if self.gpus:
+            batch = [a.to(self.gpus[0]) for a in batch]
+        return batch
+
+    def prepare_model(self, model):
+        model.trainer = self
+        model.board.xlim = [0, self.max_epochs]
+        if self.gpus:
+            model.to(self.gpus[0])
+        self.model = model
+
+    def fit(self, model, data):
+        self.prepare_data(data)
+        self.prepare_model(model)
+        self.optim = model.configure_optimizers()
+        self.epoch = 0
+        self.train_batch_idx = 0
+        self.val_batch_idx = 0
+        for self.epoch in range(self.max_epochs):
+            self.fit_epoch()
+
+    def fit_epoch(self):
+        self.model.train()
+        for batch in self.train_dataloader:
+            loss = self.model.training_step(self.prepare_batch(batch))
+            self.optim.zero_grad()
+            with torch.no_grad():
+                loss.backward()
+                if self.gradient_clip_val > 0:
+                    self.clip_gradients(self.gradient_clip_val, self.model)
+                self.optim.step()
+            self.train_batch_idx += 1
+        if self.val_dataloader is None:
+            return
+        self.model.eval()
+        for batch in self.val_dataloader:
+            with torch.no_grad():
+                self.model.validation_step(self.prepare_batch(batch))
+            self.val_batch_idx += 1
